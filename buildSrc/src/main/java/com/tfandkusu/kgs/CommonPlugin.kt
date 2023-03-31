fd9bb69e0d62babd27232793887630993339aa45
Haruhiko Takada <haruhiko.takada@gmail.com>
2023-03-31 17:55:39 +0900

    sourceSetsの共通部分をプラグインに移動

--- buildSrc/src/main/java/com/tfandkusu/kgs/CommonPlugin.kt
+++ buildSrc/src/main/java/com/tfandkusu/kgs/CommonPlugin.kt
@@ -3,21 +3,50 @@ package com.tfandkusu.kgs
 import com.android.build.gradle.BaseExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.kotlin
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 
 class CommonPlugin : Plugin<Project> {
     override fun apply(project: Project) {
+        // Androidの設定
         project.extensions.findByType(BaseExtension::class.java)?.let {
             it.compileSdkVersion(33)
             it.defaultConfig.minSdk = 21
             it.defaultConfig.targetSdk = 33
         }
         project.extensions.findByType(KotlinMultiplatformExtension::class.java)?.let {
+            // Androidの設定
             it.android().compilations.all {
                 kotlinOptions {
                     jvmTarget = "1.8"
                 }
             }
+            // iOS向けビルドを作れるようにする
+            listOf(
+                it.iosX64(),
+                it.iosArm64(),
+                it.iosSimulatorArm64()
+            ).forEach {
+                it.binaries.framework {
+                    baseName = project.name
+                }
+            }
+            // commonTestの設定
+            it.sourceSets.getByName("commonTest").dependencies {
+                implementation(kotlin("test"))
+            }
+            // iOSの設定
+            it.sourceSets.create("iosMain").dependsOn(it.sourceSets.getByName("commonMain"))
+            val iosMain = it.sourceSets.getByName("iosMain")
+            it.sourceSets.getByName("iosX64Main").dependsOn(iosMain)
+            it.sourceSets.getByName("iosArm64Main").dependsOn(iosMain)
+            it.sourceSets.getByName("iosSimulatorArm64Main").dependsOn(iosMain)
+            // iOSのテストの設定
+            it.sourceSets.create("iosTest").dependsOn(it.sourceSets.getByName("commonTest"))
+            val iosTest = it.sourceSets.getByName("iosTest")
+            it.sourceSets.getByName("iosX64Test").dependsOn(iosTest)
+            it.sourceSets.getByName("iosArm64Test").dependsOn(iosTest)
+            it.sourceSets.getByName("iosSimulatorArm64Test").dependsOn(iosTest)
         }
     }
 }
--- data/remote/build.gradle.kts
+++ data/remote/build.gradle.kts
@@ -9,16 +9,6 @@ plugins {
 apply<CommonPlugin>()
 
 kotlin {
-    listOf(
-        iosX64(),
-        iosArm64(),
-        iosSimulatorArm64()
-    ).forEach {
-        it.binaries.framework {
-            baseName = "remote"
-        }
-    }
-
     sourceSets {
         val commonMain by getting {
             dependencies {
@@ -33,7 +23,6 @@ kotlin {
         }
         val commonTest by getting {
             dependencies {
-                implementation(kotlin("test"))
                 implementation(libs.kotest.assertions)
             }
         }
@@ -42,27 +31,10 @@ kotlin {
                 implementation(libs.ktor.client.okhttp)
             }
         }
-        val androidUnitTest by getting
-        val iosX64Main by getting
-        val iosArm64Main by getting
-        val iosSimulatorArm64Main by getting
-        val iosMain by creating {
-            dependsOn(commonMain)
+        val iosMain by getting {
             dependencies {
                 implementation(libs.ktor.client.darwin)
             }
-            iosX64Main.dependsOn(this)
-            iosArm64Main.dependsOn(this)
-            iosSimulatorArm64Main.dependsOn(this)
-        }
-        val iosX64Test by getting
-        val iosArm64Test by getting
-        val iosSimulatorArm64Test by getting
-        val iosTest by creating {
-            dependsOn(commonTest)
-            iosX64Test.dependsOn(this)
-            iosArm64Test.dependsOn(this)
-            iosSimulatorArm64Test.dependsOn(this)
         }
     }
 }
