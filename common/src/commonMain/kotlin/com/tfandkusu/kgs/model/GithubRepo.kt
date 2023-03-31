5d8ed581677c354ebb0c7bd69f44db3621367265
Haruhiko Takada <haruhiko.takada@gmail.com>
2023-03-31 19:33:15 +0900

    GithubRepo にコメント追加

--- common/src/commonMain/kotlin/com/tfandkusu/kgs/model/GithubRepo.kt
+++ common/src/commonMain/kotlin/com/tfandkusu/kgs/model/GithubRepo.kt
@@ -1,5 +1,18 @@
 package com.tfandkusu.kgs.model
 
+/**
+ * GitHubレポジトリ
+ *
+ * @param id ID
+ * @param fullName リポジトリ名(例 tfandkusu/android_app_template)
+ * @param login 作成者
+ * @param avatarUrl 作成者のアバターURL
+ * @param language プログラミング言語
+ * @param stargazersCount スター数
+ * @param watchersCount Watcher数
+ * @param forksCount フォーク数
+ * @param openIssuesCount OpenなIssue数
+ */
 data class GithubRepo(
     val id: Long,
     val fullName: String,
