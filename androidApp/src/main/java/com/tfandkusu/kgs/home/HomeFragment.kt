3720c8aff94ceeb0a7eb6f1559124f5e68291f6c
Haruhiko Takada <haruhiko.takada@gmail.com>
2023-04-01 19:05:15 +0900

    ホーム画面を設置

--- androidApp/src/main/java/com/tfandkusu/kgs/home/HomeFragment.kt
+++ androidApp/src/main/java/com/tfandkusu/kgs/home/HomeFragment.kt
@@ -4,15 +4,24 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.fragment.app.Fragment
-import com.tfandkusu.kgs.R
+import com.tfandkusu.kgs.MyTheme
 
 class HomeFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        return ComposeView(requireContext()).apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                MyTheme {
+                    HomeScreen()
+                }
+            }
+        }
     }
 }
