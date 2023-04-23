e25b4941380da431c482fb7c70df6d70f74813ba
Haruhiko Takada <haruhiko.takada@gmail.com>
2023-04-23 21:09:43 +0900

    HomeEvent.InputKeyword まで Mockmp でテストを書いた。

--- feature/home/build.gradle.kts
+++ feature/home/build.gradle.kts
@@ -1,15 +1,19 @@
 @file:Suppress("UnusedImport")
-
 import com.tfandkusu.kgs.CommonPlugin
 
 plugins {
     kotlin("multiplatform")
     id("com.android.library")
+    id("com.google.devtools.ksp").version("1.8.0-1.0.9")
+    id("org.kodein.mock.mockmp") version "1.12.0"
 }
 apply<CommonPlugin>()
 
 android {
     namespace = "com.tfandkusu.kgs.feature.home"
+}
+mockmp {
+    usesHelper = true
 }
 
 kotlin {
--- feature/home/src/commonTest/kotlin/com/tfandkusu/kgs/feature/home/HomeActionCreatorTest.kt
+++ feature/home/src/commonTest/kotlin/com/tfandkusu/kgs/feature/home/HomeActionCreatorTest.kt
@@ -1,21 +1,44 @@
 package com.tfandkusu.kgs.feature.home
 
-import io.kotest.matchers.shouldBe
+import com.tfandkusu.kgs.data.remote.GithubRemoteDataSource
+import com.tfandkusu.kgs.feature.viewmodel.Dispatcher
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.runTest
+import org.kodein.mock.Mock
+import org.kodein.mock.tests.TestsWithMocks
 import kotlin.test.BeforeTest
 import kotlin.test.Test
 
 @OptIn(ExperimentalCoroutinesApi::class)
-class HomeActionCreatorTest {
+class HomeActionCreatorTest : TestsWithMocks() {
+    /**
+     * injectMocks は KSP によって作られる
+     */
+    override fun setUpMocks() = injectMocks(mocker) //(1)
+
+    @Mock
+    lateinit var dispatcher: Dispatcher<HomeAction>
+
+    @Mock
+    lateinit var remoteDataSource: GithubRemoteDataSource
+
+    private val actionCreator by withMocks {
+        HomeActionCreator(remoteDataSource)
+    }
 
     @BeforeTest
-    fun setUp() {
-
+    fun setUp() = runBlocking {
+        everySuspending {
+            dispatcher.dispatch(isAny())
+        } returns Unit
     }
 
     @Test
     fun inputKeyword() = runTest {
-        1 shouldBe 1
+        actionCreator.event(HomeEvent.InputKeyword("Kotlin"), dispatcher)
+        verifyWithSuspend {
+            dispatcher.dispatch(HomeAction.UpdateKeyword("Kotlin"))
+        }
     }
 }
