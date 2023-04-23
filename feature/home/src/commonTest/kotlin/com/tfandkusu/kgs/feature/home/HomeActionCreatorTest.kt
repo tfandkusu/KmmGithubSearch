d646a0f2f2ef40c27b11be8331dfeb9732e9bd05
Haruhiko Takada <haruhiko.takada@gmail.com>
2023-04-23 23:41:00 +0900

    Revert "HomeEvent.InputKeyword まで Mockmp でテストを書いた。"
    This reverts commit e25b4941380da431c482fb7c70df6d70f74813ba.

--- feature/home/src/commonTest/kotlin/com/tfandkusu/kgs/feature/home/HomeActionCreatorTest.kt
+++ feature/home/src/commonTest/kotlin/com/tfandkusu/kgs/feature/home/HomeActionCreatorTest.kt
@@ -1,44 +1,21 @@
 package com.tfandkusu.kgs.feature.home
 
-import com.tfandkusu.kgs.data.remote.GithubRemoteDataSource
-import com.tfandkusu.kgs.feature.viewmodel.Dispatcher
+import io.kotest.matchers.shouldBe
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.runTest
-import org.kodein.mock.Mock
-import org.kodein.mock.tests.TestsWithMocks
 import kotlin.test.BeforeTest
 import kotlin.test.Test
 
 @OptIn(ExperimentalCoroutinesApi::class)
-class HomeActionCreatorTest : TestsWithMocks() {
-    /**
-     * injectMocks は KSP によって作られる
-     */
-    override fun setUpMocks() = injectMocks(mocker) //(1)
-
-    @Mock
-    lateinit var dispatcher: Dispatcher<HomeAction>
-
-    @Mock
-    lateinit var remoteDataSource: GithubRemoteDataSource
-
-    private val actionCreator by withMocks {
-        HomeActionCreator(remoteDataSource)
-    }
+class HomeActionCreatorTest {
 
     @BeforeTest
-    fun setUp() = runBlocking {
-        everySuspending {
-            dispatcher.dispatch(isAny())
-        } returns Unit
+    fun setUp() {
+
     }
 
     @Test
     fun inputKeyword() = runTest {
-        actionCreator.event(HomeEvent.InputKeyword("Kotlin"), dispatcher)
-        verifyWithSuspend {
-            dispatcher.dispatch(HomeAction.UpdateKeyword("Kotlin"))
-        }
+        1 shouldBe 1
     }
 }
