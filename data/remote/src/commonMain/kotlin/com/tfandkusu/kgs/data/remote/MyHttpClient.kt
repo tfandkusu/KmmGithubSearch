c032f4e610366a5c2b95225db3efd0e22d6f21b4
Haruhiko Takada <haruhiko.takada@gmail.com>
2023-04-30 05:40:56 +0900

    ContentNegotiation 設定をひとつにまとめた。

--- data/remote/src/commonMain/kotlin/com/tfandkusu/kgs/data/remote/MyHttpClient.kt
+++ data/remote/src/commonMain/kotlin/com/tfandkusu/kgs/data/remote/MyHttpClient.kt
@@ -1,5 +1,20 @@
 package com.tfandkusu.kgs.data.remote
 
 import io.ktor.client.HttpClient
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonNamingStrategy
 
 expect fun getMyHttpClient(): HttpClient
+
+@OptIn(ExperimentalSerializationApi::class)
+fun ContentNegotiation.Config.myConfigure(){
+    json(
+        Json {
+            ignoreUnknownKeys = true
+            namingStrategy = JsonNamingStrategy.SnakeCase
+        }
+    )
+}
--- data/remote/src/iosMain/kotlin/com/tfandkusu/kgs/data/remote/GetMyHttpClient.kt
+++ data/remote/src/iosMain/kotlin/com/tfandkusu/kgs/data/remote/GetMyHttpClient.kt
@@ -5,22 +5,13 @@ import io.ktor.client.HttpClient
 import io.ktor.client.engine.darwin.Darwin
 import io.ktor.client.plugins.HttpResponseValidator
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
-import io.ktor.serialization.kotlinx.json.json
-import kotlinx.serialization.ExperimentalSerializationApi
-import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.JsonNamingStrategy
 
-@OptIn(ExperimentalSerializationApi::class)
 actual fun getMyHttpClient(): HttpClient {
     return HttpClient(Darwin) {
-        install(ContentNegotiation) {
-            json(
-                Json {
-                    ignoreUnknownKeys = true
-                    namingStrategy = JsonNamingStrategy.SnakeCase
-                },
-            )
-        }
+        install(
+            plugin = ContentNegotiation,
+            configure = { myConfigure() }
+        )
         HttpResponseValidator {
             validateResponse { response ->
                 if (response.status.value in 200..299) {
