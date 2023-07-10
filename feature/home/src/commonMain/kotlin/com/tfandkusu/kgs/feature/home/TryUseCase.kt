d15d6abc6611d88956b516cdfbe2e32b19cbe398
Haruhiko Takada <haruhiko.takada@gmail.com>
2023-07-11 00:36:54 +0900

    Swift で直接呼び出すメソッドだけ `@Throws` が必要だった。

--- feature/home/src/commonMain/kotlin/com/tfandkusu/kgs/feature/home/TryUseCase.kt
+++ feature/home/src/commonMain/kotlin/com/tfandkusu/kgs/feature/home/TryUseCase.kt
@@ -6,11 +6,21 @@ import kotlin.coroutines.cancellation.CancellationException
 object UnknownException : Exception()
 
 class TryUseCase {
+
+    private val repository = TryRepository()
+
     @Throws(
         CancellationException::class,
         UnknownException::class,
     )
     suspend fun execute(): Int {
+        return repository.get()
+    }
+}
+
+class TryRepository {
+
+    suspend fun get(): Int {
         delay(1000)
         throw UnknownException
         return 1
