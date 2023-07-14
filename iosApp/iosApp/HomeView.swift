ef9a0b4d48b0c753fdb0999c7c855370d00deb5e
Haruhiko Takada <haruhiko.takada@gmail.com>
2023-07-14 18:59:47 +0900

    Swift 側にもコメントを付ける。

--- iosApp/iosApp/HomeView.swift
+++ iosApp/iosApp/HomeView.swift
@@ -18,16 +18,19 @@ struct HomeView: View {
             Text(String(format: "検索結果は%d件", viewModel.state.items.count)).font(.body).padding(8)
             Spacer()
         }.navigationBarTitle("GitHubリポジトリ検索").onAppear(perform: {
+            // KMP-NativeCoroutines を使い
+            // Swift から Kotlin の suspend 関数を呼ぶ実験
             Task {
                 let useCase = ExampleUseCase()
                 do {
                     let result = try await asyncFunction(for: useCase.execute())
                     print("result = \(result)")
                 } catch {
+                    // Kotlin の例外オブジェクトを取得する
                     let nsError = error as NSError
                     let kotlinException = nsError.userInfo["KotlinException"]
                     if let nonOpticalKotlinException = kotlinException {
-                        if(kotlinException is ExampleException) {
+                        if(nonOpticalKotlinException is ExampleException) {
                             let exampleException = nonOpticalKotlinException as! ExampleException
                             let code = exampleException.code
                             print("code = \(code)")
