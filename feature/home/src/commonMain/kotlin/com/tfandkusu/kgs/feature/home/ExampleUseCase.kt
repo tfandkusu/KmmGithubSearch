8953fe59d66d65cccdbdb0894098cf17a6127edb
Haruhiko Takada <haruhiko.takada@gmail.com>
2023-07-14 18:12:14 +0900

    検証用とわかりやすい名前に変更

--- feature/home/src/commonMain/kotlin/com/tfandkusu/kgs/feature/home/ExampleUseCase.kt
+++ feature/home/src/commonMain/kotlin/com/tfandkusu/kgs/feature/home/ExampleUseCase.kt
@@ -3,26 +3,26 @@ package com.tfandkusu.kgs.feature.home
 import kotlinx.coroutines.delay
 import kotlin.coroutines.cancellation.CancellationException
 
-object UnknownException : Exception()
+object ExampleException : Exception()
 
-class TryUseCase {
+class ExampleUseCase {
 
-    private val repository = TryRepository()
+    private val repository = ExampleRepository()
 
     @Throws(
         CancellationException::class,
-        UnknownException::class,
+        ExampleException::class,
     )
     suspend fun execute(): Int {
         return repository.get()
     }
 }
 
-class TryRepository {
+class ExampleRepository {
 
     suspend fun get(): Int {
         delay(1000)
-        throw UnknownException
+        throw ExampleException
         return 1
     }
 }
