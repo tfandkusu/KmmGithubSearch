f9a4eb8ececabfab32eb74ffdc303d79b14af805
Haruhiko Takada <haruhiko.takada@gmail.com>
2023-08-19 21:26:36 +0900

    ビルド時間をBigQueryにアップロードできるようにした。

--- buildSrc/src/main/java/com/tfandkusu/kgs/BuildDurationUploadAction.kt
+++ buildSrc/src/main/java/com/tfandkusu/kgs/BuildDurationUploadAction.kt
@@ -1,5 +1,8 @@
 package com.tfandkusu.kgs
 
+import com.google.cloud.Timestamp
+import com.google.cloud.bigquery.BigQueryOptions
+import com.google.cloud.bigquery.InsertAllRequest
 import org.gradle.api.flow.FlowAction
 import org.gradle.api.flow.FlowParameters
 import org.gradle.api.tasks.Input
@@ -13,6 +16,28 @@ class BuildDurationUploadAction : FlowAction<BuildDurationUploadAction.Parameter
     }
 
     override fun execute(parameters: Parameters) {
-        println("Build duration = " + parameters.getBuildDuration().get())
+        val bigquery = BigQueryOptions.newBuilder().setProjectId(PROJECT_ID).build().service
+        val timestamp = Timestamp.now().toString()
+        val duration = parameters.getBuildDuration().get()
+        val type = "incremental"
+        val content = mapOf<String, Any>(
+            "created_at" to timestamp,
+            "duration" to duration,
+            "type" to type
+        )
+        val request = InsertAllRequest.newBuilder(
+            DATASET_NAME,
+            TABLE_NAME
+        ).addRow(content).build()
+        val response = bigquery.insertAll(request)
+        if (response.hasErrors()) {
+            throw RuntimeException(response.insertErrors.toString())
+        }
+    }
+
+    companion object {
+        const val PROJECT_ID = "tfandkusu"
+        const val DATASET_NAME = "kmm_github_search"
+        const val TABLE_NAME = "build_time"
     }
 }
