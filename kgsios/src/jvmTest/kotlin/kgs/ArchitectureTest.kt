955a1ddfb801dd11bfb1baff5cb34855baddd2cc
Haruhiko Takada <haruhiko.takada@gmail.com>
2023-09-09 18:57:08 +0900

    iOS 向け DI 設定の抜けを検出する

--- kgsios/src/jvmTest/kotlin/kgs/ArchitectureTest.kt
+++ kgsios/src/jvmTest/kotlin/kgs/ArchitectureTest.kt
@@ -3,6 +3,7 @@ package kgs
 import com.lemonappdev.konsist.api.Konsist
 import com.lemonappdev.konsist.api.ext.list.withNameEndingWith
 import com.lemonappdev.konsist.api.verify.assert
+import org.junit.Assert
 import org.junit.Test
 
 class ArchitectureTest {
@@ -12,5 +13,24 @@ class ArchitectureTest {
             .classes()
             .withNameEndingWith("RemoteDataSourceImpl")
             .assert { it.hasInternalModifier }
+    }
+
+    @Test
+    fun checkAllActionCreatorsAreRegisteredInHelper() {
+        val registeredClassNames = Konsist.scopeFromProject().classes()
+            .withNameEndingWith("ViewModelHelper")
+            .flatMap {
+                it.properties()
+            }.map {
+                it.type?.name ?: ""
+            }.toSet()
+        val actionCreators = Konsist.scopeFromProject().classes()
+            .withNameEndingWith("ActionCreator")
+        val reducers = Konsist.scopeFromProject().classes()
+            .withNameEndingWith("Reducer")
+        val checkClassNames = (actionCreators + reducers).map {
+            it.name
+        }.toSet()
+        Assert.assertEquals(checkClassNames, registeredClassNames)
     }
 }
