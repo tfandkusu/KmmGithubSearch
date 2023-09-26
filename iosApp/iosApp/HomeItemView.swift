424d22e1a6ae4613d7d4294ee6af69041f3b39d0
Haruhiko Takada <haruhiko.takada@gmail.com>
2023-09-26 22:18:36 +0900

    サーバエラーの時の処理を修正

--- iosApp/iosApp/HomeItemView.swift
+++ iosApp/iosApp/HomeItemView.swift
@@ -10,14 +10,14 @@ struct HomeItemView: View {
             HStack(alignment: .center) {
                 ProgressView().padding(EdgeInsets(top: 8, leading: 16, bottom: 8, trailing: 16))
             }.frame(maxWidth: .infinity)
-        case let .repo(item):
-            Text(item.repo.fullName).padding(EdgeInsets(top: 8, leading: 16, bottom: 8, trailing: 16))
+        case let .repo(data):
+            Text(data.repo.fullName).padding(EdgeInsets(top: 8, leading: 16, bottom: 8, trailing: 16))
         case .networkError:
             Text(NSLocalizedString("network error", comment: "network error"))
                 .padding(EdgeInsets(top: 8, leading: 16, bottom: 8, trailing: 16))
-        case let .serverError(statusCode):
+        case let .serverError(data):
             let format = NSLocalizedString("server error", comment: "network error")
-            Text(String.localizedStringWithFormat(format, statusCode))
+            Text(String.localizedStringWithFormat(format, data.statusCode))
                 .padding(EdgeInsets(top: 8, leading: 16, bottom: 8, trailing: 16))
         }
     }
