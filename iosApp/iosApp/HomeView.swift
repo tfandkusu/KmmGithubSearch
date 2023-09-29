80d9166e1d1438fb952cdf9722236d8042df15f8
Haruhiko Takada <haruhiko.takada@gmail.com>
2023-09-30 00:24:16 +0900

    KMP NativeCoroutines をいったんコメントアウト

--- iosApp/iosApp/HomeView.swift
+++ iosApp/iosApp/HomeView.swift
@@ -23,24 +23,25 @@ struct HomeView: View {
                 }
             }
         }.navigationBarTitle("GitHubリポジトリ検索").onAppear(perform: {
-            // KMP-NativeCoroutines を使い
             // Swift から Kotlin の suspend 関数を呼ぶ実験
-            Task {
+            Task.detached {
                 let useCase = ExampleUseCase()
+
                 do {
-                    let result = try await asyncFunction(for: useCase.execute())
+                    let result = try? await useCase.execute()
                     print("result = \(result)")
                 } catch {
+                    print(error)
                     // Kotlin の例外オブジェクトを取得する
-                    let nsError = error as NSError
-                    let kotlinException = nsError.userInfo["KotlinException"]
-                    if let nonOpticalKotlinException = kotlinException {
-                        if nonOpticalKotlinException is ExampleException {
-                            let exampleException = nonOpticalKotlinException as! ExampleException
-                            let code = exampleException.code
-                            print("code = \(code)")
-                        }
-                    }
+//                    let nsError = error as NSError
+//                    let kotlinException = nsError.userInfo["KotlinException"]
+//                    if let nonOpticalKotlinException = kotlinException {
+//                        if nonOpticalKotlinException is ExampleException {
+//                            let exampleException = nonOpticalKotlinException as! ExampleException
+//                            let code = exampleException.code
+//                            print("code = \(code)")
+//                        }
+//                    }
                 }
             }
         })
