caf29515f3412e3974ed63492cb67d068d371b36
Haruhiko Takada <haruhiko.takada@gmail.com>
2023-09-30 01:29:49 +0900

    まず Android で UseCase クラスを使う

--- feature/home/src/commonMain/kotlin/com/tfandkusu/kgs/di/FeatureHomeModule.kt
+++ feature/home/src/commonMain/kotlin/com/tfandkusu/kgs/di/FeatureHomeModule.kt
@@ -2,11 +2,14 @@ package com.tfandkusu.kgs.di
 
 import com.tfandkusu.kgs.feature.home.HomeActionCreator
 import com.tfandkusu.kgs.feature.home.HomeReducer
+import com.tfandkusu.kgs.feature.home.SearchGithub
+import com.tfandkusu.kgs.feature.home.SearchGithubUseCase
 import org.koin.dsl.module
 
 val dataRemoteModuleInFeatureHomeModule = dataRemoteModule
 
 val featureHomeModule = module {
+    factory<SearchGithubUseCase> { SearchGithub(get()) }
     factory { HomeActionCreator(get()) }
     single { HomeReducer() }
 }
