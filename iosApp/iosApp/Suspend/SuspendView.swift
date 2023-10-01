2ea370496d859294704750b8ea2b00ebbf0465f5
Haruhiko Takada <haruhiko.takada@gmail.com>
2023-10-02 02:46:15 +0900

    キャンセルの動作確認まで完了

--- iosApp/iosApp/Suspend/SuspendView.swift
+++ iosApp/iosApp/Suspend/SuspendView.swift
@@ -5,7 +5,36 @@ struct SuspendView: View {
     let store: StoreOf<SuspendReducer>
 
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-            .navigationBarTitle("suspend 関数動作確認")
+        WithViewStore(self.store, observe: { $0 }) { viewStore in
+            VStack {
+                HStack {
+                    Button(
+                        action: {
+                            viewStore.send(.callFunction)
+                        }
+                    ) {
+                        Text("関数呼び出し")
+                    }.padding(16)
+                    Button(
+                        action: {
+                            viewStore.send(.cancelFunction)
+                        }
+                    ) {
+                        Text("関数キャンセル")
+                    }.padding(16)
+                }
+                switch viewStore.status {
+                case .initial:
+                    EmptyView()
+                case .running:
+                    Text("suspend 関数動作中")
+                case .completed:
+                    Text("suspend 関数完了")
+                case .canceled:
+                    Text("suspend 関数はキャンセルされた")
+                }
+                Spacer()
+            }.navigationBarTitle("suspend 関数動作確認")
+        }
     }
 }
