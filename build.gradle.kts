511f40448526105687acb63fc2443cf045f5961b
Haruhiko Takada <haruhiko.takada@gmail.com>
2024-04-07 05:12:27 +0900

    DeteKt の導入

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     id("com.google.devtools.ksp") version "1.9.20-1.0.14"
     alias(libs.plugins.spotless.gradle.plugin)
     id("co.touchlab.skie") version "0.5.5"
+    id("io.gitlab.arturbosch.detekt") version("1.23.3")
 }
 
 koverReport {
@@ -23,5 +24,11 @@ spotless {
     kotlin {
         target("**/*.kt")
         ktlint("0.50.0").userData(mapOf("android" to "true"))
+    }
+}
+
+subprojects {
+    apply(plugin = "io.gitlab.arturbosch.detekt")
+    detekt {
     }
 }
