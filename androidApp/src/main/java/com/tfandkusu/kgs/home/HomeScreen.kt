3a9f223cdffdbaa57f75e0211af6e0e85bb804f8
Haruhiko Takada <haruhiko.takada@gmail.com>
2024-05-30 02:32:06 +0900

    わざと差分を作る

--- androidApp/src/main/java/com/tfandkusu/kgs/home/HomeScreen.kt
+++ androidApp/src/main/java/com/tfandkusu/kgs/home/HomeScreen.kt
@@ -41,7 +41,6 @@ import androidx.compose.ui.unit.dp
 import com.tfandkusu.kgs.R
 import com.tfandkusu.kgs.compose.MyTheme
 import com.tfandkusu.kgs.compose.MyTopAppBar
-import com.tfandkusu.kgs.compose.TrackScreenEvent
 import com.tfandkusu.kgs.feature.home.HomeEffect
 import com.tfandkusu.kgs.feature.home.HomeEvent
 import com.tfandkusu.kgs.feature.home.HomeState
@@ -60,7 +59,7 @@ fun HomeScreen(viewModel: HomeViewModel) {
     val (state, effect, dispatch) = use(viewModel)
     val focusRequester = remember { FocusRequester() }
     val focusManager = LocalFocusManager.current
-    TrackScreenEvent("Home")
+    // わざと差分を作る
     LaunchedEffect(Unit) {
         effect.collect {
             when (it) {
